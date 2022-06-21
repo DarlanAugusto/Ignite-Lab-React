66cb5787912eb008fe4fe791266e30eeefa32ce2
Darlan Silva <darlansilv4@gmail.com>
2022-06-21 11:14:46 -0300

    Instalando TailwindCss e PostCss

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
+        <p>Hello Ignite Lab</p>
         <p>
           <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
+            contador é: {count}
           </button>
         </p>
         <p>
